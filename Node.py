4672a07d95276c52cde6f2285a498fe0f307d048
Amit Verma <amit.v@hotmail.com>
2020-10-27 16:23:34 -0400

    separated node class into its own module

---
+++ Node.py
@@ -0,0 +1,72 @@
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQ = (64, 224, 208)
+
+class Node:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbours = []
+        self.width = width
+        self.total_rows = total_rows
+    
+    def get_position(self):
+        return self.row, self.col
+
+    def is_closed(self):
+        return self.color == RED
+
+    def is_open(self):
+        return self.color == GREEN
+
+    def is_barrier(self):
+        return self.color == BLACK
+
+    def is_start(self):
+        return self.color == ORANGE
+
+    def is_end(self):
+        return self.color == TURQ
+
+    def is_path(self):
+        return self.color == PURPLE
+
+    def reset(self):
+        return self.color == WHITE
+
+    def make_start(self):
+        self.color = ORANGE
+
+    def make_closed(self):
+        self.color = RED
+
+    def make_open(self):
+        self.color = GREEN
+
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_end(self):
+        self.color = TURQ
+
+    def make_path(self):
+        self.color = PURPLE
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+    
+    def update_neighbours(self, grid):
+        pass
+    
+    def __lt__(self, other):
+        return False
--- astar.py
+++ astar.py
@@ -1,81 +1,24 @@
 import pygame
 import math
 from queue import PriorityQueue
+from Node import Node
 
 SIDE = 800
 win = pygame.display.set_mode((SIDE, SIDE))
 pygame.display.set_caption("A* Path Finder")
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 255, 0)
-YELLOW = (255, 255, 0)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-PURPLE = (128, 0, 128)
-ORANGE = (255, 165 ,0)
-GREY = (128, 128, 128)
-TURQ = (64, 224, 208)
 
-class Node:
-    def __init__(self, row, col, width, total_rows):
-        self.row = row
-        self.col = col
-        self.x = row * width
-        self.y = col * width
-        self.color = WHITE
-        self.neighbours = []
-        self.width = width
-        self.total_rows = total_rows
-    
-    def get_position(self):
-        return self.row, self.col
+def h(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
 
-    def is_closed(self):
-        return self.color == RED
+    return abs(x2 - x1) + abs(y2 - y1)
 
-    def is_open(self):
-        return self.color == GREEN
+def gen_grid(rows, width):
+    grid = []
+    gap = width // rows
 
-    def is_barrier(self):
-        return self.color == BLACK
-
-    def is_start(self):
-        return self.color == ORANGE
-
-    def is_end(self):
-        return self.color == TURQ
-
-    def is_path(self):
-        return self.color == PURPLE
-
-    def reset(self):
-        return self.color == WHITE
-
-    def make_start(self):
-        self.color = ORANGE
-
-    def make_closed(self):
-        self.color = RED
-
-    def make_open(self):
-        self.color = GREEN
-
-    def make_barrier(self):
-        self.color = BLACK
-
-    def make_end(self):
-        self.color = TURQ
-
-    def make_path(self):
-        self.color = PURPLE
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
-    
-    def update_neighbours(self, grid):
-        pass
-    
-    def __lt__(self, other):
-        return False
-
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node
