fb082bea382e3916d74cfeb7f5b93b0425c29153
Amit Verma <amit.v@hotmail.com>
2020-10-27 14:21:17 -0400

    added methods to change color of nodes and return color of nodes

--- astar.py
+++ astar.py
@@ -6,6 +6,17 @@ SIDE = 800
 win = pygame.display.set_mode((SIDE, SIDE))
 pygame.display.set_caption("A* Path Finder")
 
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQ = (64, 224, 208)
+
 class Node:
     def __init__(self, row, col, width, total_rows):
         self.row = row
@@ -17,26 +28,44 @@ class Node:
         self.width = width
         self.total_rows = total_rows
     
-    def get_position(self)
+    def get_position(self):
         return self.row, self.col
 
     def is_closed(self):
         return self.color == RED
-    
+
     def is_open(self):
         return self.color == GREEN
 
     def is_barrier(self):
         return self.color == BLACK
-    
+
     def is_start(self):
         return self.color == ORANGE
-    
+
     def is_end(self):
         return self.color == TURQ
-    
+
     def is_path(self):
         return self.color == PURPLE
-    
+
     def reset(self):
         return self.color == WHITE
+
+    def make_start(self):
+        self.color = ORANGE
+
+    def make_closed(self):
+        self.color = RED
+
+    def make_open(self):
+        self.color = GREEN
+
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_end(self):
+        self.color = TURQ
+
+    def make_path(self):
+        self.color = PURPLE
